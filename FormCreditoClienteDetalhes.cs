0f163db6338aa420e10140ac865a7ffe4a731707
edsonlcandido <edson.luiz.candido@gmail.com>
2022-07-19 23:29:01 -0300

    Adicionado FormCreditoClienteDetalhes para ser aberto no FormClienteDetalhes

---
+++ FormCreditoClienteDetalhes.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace CreditoCliente
+{
+    public partial class FormCreditoClienteDetalhes : Form
+    {
+        public FormCreditoClienteDetalhes()
+        {
+            InitializeComponent();
+        }
+    }
+}

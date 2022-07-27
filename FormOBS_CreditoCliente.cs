af478c1bb70c45fc2e82496380dd5f6c64b1f2e6
edsonlcandido <edson.luiz.candido@gmail.com>
2022-07-26 21:37:52 -0300

    desenhado form Observação

---
+++ FormOBS_CreditoCliente.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace CreditoCliente
+{
+    public partial class FormOBS_CreditoCliente : Form
+    {
+        public FormOBS_CreditoCliente()
+        {
+            InitializeComponent();
+        }
+    }
+}
